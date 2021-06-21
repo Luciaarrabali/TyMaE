232a0ebfde31a7219f335a445e506f44fd3899b4
carmenarrabali@gmail.com <carmenarrabali@gmail.com>
2021-06-21 12:10:43 +0200

    New File Read Fastas

---
+++ src/readFasta.py
@@ -0,0 +1,12 @@
+# Leer un archivo FASTA
+def readFasta(FASTA):
+    f = open(FASTA, "r")
+    lines = f.readlines()
+    seq = ""
+
+    for i in lines:
+        if ">" in i:
+            pass
+        else:
+            seq += i.strip()
+    return(seq)
